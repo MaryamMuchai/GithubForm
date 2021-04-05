3e75b78acffddbee394aee083a141854580149f0
Marya <maryammuchai@gmail.com>
2021-04-05 20:58:02 +0300

    adding the client key

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor( ) { }
 
   ngOnInit(): void {
   }
--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -8,9 +8,32 @@ import { Observable, throwError } from 'rxjs';
 })
 export class GithubService {
   [x: string]: any;
+  private userName='maryammuchai';
+  private clientId = 'e57c1b2aae512a8e0b5b'
+  private clientSecret='aeb1040acfdcd7f83895ac7940da76d9d03225e7'
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient:HttpClient) { 
 
-  
-}
+  }
+  getUser(){
+    return this._http.get('https://api.github.com/users/' + this.userName + '?client_id='
+  + this.clientId + '&client_secret=' + this.clientSecret)
+  .map((res: { json: () => any; }) => res.json());
+  }
+
+  getRepos(){
+    return this._http.get('https://api.github.com/users/' + this.userName + '/repos?client_id='
+  + this.clientId + '&client_secret=' + this.clientSecret)
+  .map((res: { json: () => any; }) => res.json());
+ }
+ updateUser(userName:string){
+   this.userName = userName;
+
+  }
+
+
+  }
+
+
+
 
