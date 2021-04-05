9eec87baf3618eacf21e712ff6dbb1f2d74b8a17
Marya <maryammuchai@gmail.com>
2021-04-05 15:51:03 +0300

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubAppComponent } from './github-app/github-app.component';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { GithubReposComponent } from './github-repos/github-repos.component'; 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubAppComponent
+    GithubAppComponent,
+    GithubProfileComponent,
+    GithubReposComponent
   ],
   imports: [
     BrowserModule,
--- src/app/github-app/github-app.component.ts
+++ src/app/github-app/github-app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../github.service';
 
 @Component({
   selector: 'app-github-app',
@@ -6,12 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./github-app.component.css']
 })
 export class GithubAppComponent implements OnInit {
-  
+ public githubUserQuery:string;
+ public githubProfile:any;
+ public githubRepos:any[];
+ public errorMessage:string; 
 
-  constructor() { }
+  constructor(private githubService:GithubService) { 
 
   public searchUser(){
-    
+    //to get the github profile
+this.githubService.getProfile(this.githubUserQuery).subscribe(next:(data)=> {
+this.githubProfile =data;
+} , error:(error) => {
+  this.errorMessage = error;
+  
+});
+//get the github repos
+this.githubService.getRepos(this.githubUserQuery).subscribe(next:(data) => {
+  this.githubRepos = data;
+} , error:(error)=> {
+  
+}
+
+
   }
 
 
