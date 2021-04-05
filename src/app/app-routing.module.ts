fefb1fdebf8fd6dab1e99138d6b91512b4585866
Marya <maryammuchai@gmail.com>
2021-04-05 18:35:14 +0300

    adding details to profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GithubProfileComponent } from './github-profile/github-profile.component';
 import { GithubAppComponent } from './github-app/github-app.component';
